e4bde22a04feff7afb219367d382140958b1314b
Mayank Kapoor <mayankkapoor@gmail.com>
2015-08-22 19:04:56 +0530

    Initial Commit

---
+++ Airport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Airport
+//
+//  Created by Mayank Kapoor on 22/Aug/15.
+//  Copyright (c) 2015 Mayank Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
